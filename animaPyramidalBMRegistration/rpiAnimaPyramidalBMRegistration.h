33040e99b80f8ab7ff099c19d4883788e70fb9c1
Guillaume Pasquier <guillaume.pasquier@inria.fr>
2013-01-03 15:04:22 +0100

    animaPyramidalBMRegistration plugin added

---
+++ animaPyramidalBMRegistration/rpiAnimaPyramidalBMRegistration.h
@@ -0,0 +1,155 @@
+#ifndef _RPIANIMAPYRAMIDALBMREGISTRATION_H_
+#define _RPIANIMAPYRAMIDALBMREGISTRATION_H_
+
+// This example class implements a non rigid algorithm with a displacement field transform, change for rigid or affine.
+
+#include <itkDisplacementFieldTransform.h>
+#include "rpiRegistrationMethod.hxx"
+
+#include "pyramidalMatchingBridge.h"
+
+
+// Namespace RPI : Registration Programming Interface
+namespace rpi
+{
+
+
+/**
+ * AnimaPyramidalBMRegistration registration method. This is just an interface class to your own registration method. It is here to have
+ * a common way of accessing your registration from the registration workspace.
+ *
+ * There are three templates for this class:
+ *
+ *   TFixedImage           Type of the fixed image. Must be an itk::Image< TPixel, 3 > where
+ *                         TPixel is a scalar type among "float" and "double".
+ *
+ *   TMovingImage          Type of the moving image. Must be an itk::Image< TPixel, 3 > where
+ *                         TPixel is a scalar type among "float" and "double".
+ *
+ *   TTransformScalarType  Type of the transformation parameters. Must be "float" or "double".
+ *
+ * In the current implementation, TFixedImage and TMovingImage must be identical.
+ *
+ */
+template < class TFixedImage, class TMovingImage, class TTransformScalarType=double >
+class ITK_EXPORT AnimaPyramidalBMRegistration : public RegistrationMethod< TFixedImage, TMovingImage, TTransformScalarType >
+{
+
+public:
+
+    typedef itk::DisplacementFieldTransform< TTransformScalarType, TFixedImage::ImageDimension >
+    TransformType;
+
+    typedef typename TransformType::Pointer
+    TransformPointerType;
+
+
+protected:
+
+    /**
+     * Here go the parameters of the method (number of iterations , ...)
+     */
+
+public:
+
+    /**
+     * Class constructor.
+     */
+    AnimaPyramidalBMRegistration ( void );
+
+
+    /**
+     * Class destructor.
+     */
+    virtual ~AnimaPyramidalBMRegistration ( void );
+
+
+    /**
+     * Setters and getters go here
+     */
+    void SetInitialTransform( std::string initTransformFile);
+    
+    void SetBlockSize ( int blockSize );
+
+    void SetBlockSpacing ( unsigned int blockSpacing );
+
+    void SetStDevThreshold ( float StDevThreshold );
+
+    void SetTransform ( unsigned int transform );
+
+    void SetMetric ( unsigned int metric );
+
+    void SetOptimizer ( unsigned int optimizer );
+
+    void SetMaximumIterations ( unsigned int MaximumIterations );
+
+    void SetMinimalTransformError ( float MinimalTransformError );
+
+    void SetOptimizerMaximumIterations ( unsigned int OptimizerMaximumIterations );
+
+    void SetHistogramSize ( unsigned int HistogramSize );
+
+    void SetSearchRadius ( double SearchRadius );
+
+    void SetSearchAngleRadius ( double SearchAngleRadius );
+
+    void SetSearchSkewRadius ( double SearchSkewRadius );
+
+    void SetSearchScaleRadius ( double SearchScaleRadius );
+
+    void SetFinalRadius ( double FinalRadius );
+
+    void SetStepSize ( double StepSize );
+
+    void SetTranlateUpperBound ( double TranlateUpperBound );
+
+    void SetAngleUpperBound ( double AngleUpperBound );
+
+    void SetSkewUpperBound ( double SkewUpperBound );
+
+    void SetScaleUpperBound ( double ScaleUpperBound );
+
+    void SetAgregator ( unsigned int agregator );
+
+    void SetWeightedAgregation ( bool WeightedAgregation );
+
+    void SetBlockCenteredRotations ( bool BlockCenteredRotations );
+
+    void SetOutputTransformType ( unsigned int outputTransform );
+
+    void SetAgregThreshold ( float AgregThreshold );
+
+    void SetSeStoppingThreshold ( float SeStoppingThreshold );
+
+    void SetNumberOfPyramidLevels ( unsigned int NumberOfPyramidLevels );
+
+    void SetLastPyramidLevel ( unsigned int LastPyramidLevel );
+
+    void SetNumberOfThreads ( int numberOfThreads );
+
+
+    /**
+     * Performs the image registration. Must be called before GetTransformation().
+     */
+    virtual void StartRegistration ( void );
+    
+    void Abort ( void );
+    
+    void SetProgressCallback(itk::CStyleCommand::Pointer callback );
+    
+    
+private:
+    
+    typedef PyramidalMatchingBridge <TFixedImage::ImageDimension> PyramidBMType;
+    PyramidBMType *matcher;
+
+};
+
+} // End of namespace
+
+
+/** Add the source code file (template) */
+#include "rpiAnimaPyramidalBMRegistration.hxx"
+
+
+#endif // _RPIANIMAPYRAMIDALBMREGISTRATION_H_
---
+++ animaPyramidalBMRegistration/rpiAnimaPyramidalBMRegistration.hxx
@@ -0,0 +1,310 @@
+#ifndef _RPIANIMAPYRAMIDALBMREGISTRATION_HXX_
+#define _RPIANIMAPYRAMIDALBMREGISTRATION_HXX_
+
+// Namespace RPI : Registration Programming Interface
+namespace rpi
+{
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+AnimaPyramidalBMRegistration < TFixedImage, TMovingImage, TTransformScalarType >
+::AnimaPyramidalBMRegistration ( void )
+{
+    // Initialize parameters
+
+    // Initialize the transformation
+    this->m_transform = TransformType::New();
+
+    matcher = new PyramidBMType;
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::~AnimaPyramidalBMRegistration ( void )
+{
+    // Do nothing
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void
+AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::StartRegistration ( void )
+{
+
+    // Check if fixed image has been set
+    if ( this->m_fixedImage.IsNull() )
+        throw std::runtime_error ( "Fixed image has not been set." );
+
+
+    // Check if moving image has been set
+    if ( this->m_movingImage.IsNull() )
+        throw std::runtime_error ( "Moving image has not been set." );
+
+
+    // Type definition
+    typedef  typename  TransformType::ScalarType
+    ScalarType;
+
+    typedef  typename  TransformType::VectorFieldType
+    VectorFieldType;
+
+    // Local images
+    typename  TFixedImage::ConstPointer   fixedImage  = this->m_fixedImage;
+    typename  TMovingImage::ConstPointer  movingImage = this->m_movingImage;
+
+    // Set the displacement field to the transformation object
+
+    matcher->SetReferenceImage ( this->m_fixedImage );
+    matcher->SetFloatingImage ( this->m_movingImage );
+
+    
+    typedef BaseTransformAgregator < TFixedImage::ImageDimension > AgregatorType;
+    typedef itk::AffineTransform<typename AgregatorType::ScalarType,TFixedImage::ImageDimension> AffineTransformType;
+    typedef typename AffineTransformType::Pointer AffineTransformPointer;
+
+    AffineTransformPointer tmpTrsf = AffineTransformType::New();
+    tmpTrsf->SetIdentity();
+    matcher->SetOutputTransform ( tmpTrsf.GetPointer() );
+
+    matcher->Update();
+
+    this->m_transform = matcher->GetOutputTransform();
+
+    // This will be done from your registration output, using something similar to this for non rigid transforms
+    /*  static_cast< TransformType * >(this->m_transform.GetPointer())->SetParametersAsVectorField(
+          static_cast<typename VectorFieldType::ConstPointer>(matcher->GetOutputImage()));*/
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void 
+AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >::
+SetProgressCallback(itk::CStyleCommand::Pointer callback )
+{
+    matcher->SetProgressCallback(callback);    
+}
+    
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void 
+AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::Abort ( void )
+{
+    matcher->Abort(); 
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void
+AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetInitialTransform( std::string initTransformFile)
+{
+    matcher->SetInitialTransform ( initTransformFile );
+}
+    
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void
+AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetBlockSize ( int blockSize )
+{
+    matcher->SetBlockSize ( blockSize );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void
+AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetBlockSpacing ( unsigned int blockSpacing )
+{
+    matcher->SetBlockSpacing ( blockSpacing );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetStDevThreshold ( float StDevThreshold )
+{
+    matcher->SetStDevThreshold ( StDevThreshold );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetTransform ( unsigned int transform )
+{
+    matcher->SetTransform ( ( Transform ) transform );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetMetric ( unsigned int metric )
+{
+    matcher->SetMetric ( ( Metric ) metric );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetOptimizer ( unsigned int optimizer )
+{
+    matcher->SetOptimizer ( ( Optimizer ) optimizer );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetMaximumIterations ( unsigned int MaximumIterations )
+{
+    matcher->SetMaximumIterations ( MaximumIterations );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetMinimalTransformError ( float MinimalTransformError )
+{
+    matcher->SetMinimalTransformError ( MinimalTransformError );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetOptimizerMaximumIterations ( unsigned int OptimizerMaximumIterations )
+{
+    matcher->SetOptimizerMaximumIterations ( OptimizerMaximumIterations );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetHistogramSize ( unsigned int HistogramSize )
+{
+    matcher->SetHistogramSize ( HistogramSize );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetSearchRadius ( double SearchRadius )
+{
+    matcher->SetSearchRadius ( SearchRadius );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetSearchAngleRadius ( double SearchAngleRadius )
+{
+    matcher->SetSearchAngleRadius ( SearchAngleRadius );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetSearchSkewRadius ( double SearchSkewRadius )
+{
+    matcher->SetSearchSkewRadius ( SearchSkewRadius );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetSearchScaleRadius ( double SearchScaleRadius )
+{
+    matcher->SetSearchScaleRadius ( SearchScaleRadius );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetFinalRadius ( double FinalRadius )
+{
+    matcher->SetFinalRadius ( FinalRadius );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetStepSize ( double StepSize )
+{
+    matcher->SetStepSize ( StepSize );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetTranlateUpperBound ( double TranlateUpperBound )
+{
+    matcher->SetTranlateUpperBound ( TranlateUpperBound );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetAngleUpperBound ( double AngleUpperBound )
+{
+    matcher->SetAngleUpperBound ( AngleUpperBound );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetSkewUpperBound ( double SkewUpperBound )
+{
+    matcher->SetSkewUpperBound ( SkewUpperBound );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetScaleUpperBound ( double ScaleUpperBound )
+{
+    matcher->SetScaleUpperBound ( ScaleUpperBound );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetAgregator ( unsigned int agregator )
+{
+    matcher->SetAgregator ( ( Agregator ) agregator );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetWeightedAgregation ( bool WeightedAgregation )
+{
+    matcher->SetWeightedAgregation ( WeightedAgregation );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetBlockCenteredRotations ( bool BlockCenteredRotations )
+{
+    matcher->SetBlockCenteredRotations ( BlockCenteredRotations );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetOutputTransformType ( unsigned int outputTransform )
+{
+    matcher->SetOutputTransformType ( ( OutputTransform ) outputTransform );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetAgregThreshold ( float AgregThreshold )
+{
+    matcher->SetAgregThreshold ( AgregThreshold );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetSeStoppingThreshold ( float SeStoppingThreshold )
+{
+    matcher->SetSeStoppingThreshold ( SeStoppingThreshold );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetNumberOfPyramidLevels ( unsigned int NumberOfPyramidLevels )
+{
+    matcher->SetNumberOfPyramidLevels ( NumberOfPyramidLevels );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetLastPyramidLevel ( unsigned int LastPyramidLevel )
+{
+    matcher->SetLastPyramidLevel ( LastPyramidLevel );
+}
+
+template < class TFixedImage, class TMovingImage, class TTransformScalarType >
+void AnimaPyramidalBMRegistration< TFixedImage, TMovingImage, TTransformScalarType >
+::SetNumberOfThreads ( int numberOfThreads )
+{
+    matcher->SetNumberOfThreads ( numberOfThreads );
+}
+
+} // End of namespace
+
+
+#endif // _RPIANIMAPYRAMIDALBMREGISTRATION_HXX_
